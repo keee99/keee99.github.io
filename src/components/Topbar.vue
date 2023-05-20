fa89811d34b9382f8aa35a60b7021cd500562a1e
keee99 <kohjj99@gmail.com>
2023-05-21 01:41:01 +0800

    Add transition for topbar hover effects. TODO: refactor to base.css with color scheme

--- src/components/Topbar.vue
+++ src/components/Topbar.vue
@@ -1,9 +1,19 @@
 <script setup lang="ts">
-import { RouterLink } from 'vue-router';
-import { ref } from "vue";
+import { RouterLink, useRoute } from 'vue-router';
+import { ref, watch } from "vue";
 
 const navStartItems = ref(["about", "portfolio"]);
 const navEndItems = ref(["contact"]);
+
+const route = useRoute();
+const activeItem = ref(route.fullPath);
+
+watch(
+  () => route.fullPath,
+  async () => activeItem.value = route.fullPath
+);
+
+
 
 const capitalize = (str: String) => str.charAt(0).toUpperCase() + str.slice(1);
 </script>
@@ -15,13 +25,17 @@ const capitalize = (str: String) => str.charAt(0).toUpperCase() + str.slice(1);
         <div class="topbar-start">
           <RouterLink class="topbar-title" to="/">JJ</RouterLink>
           
-          <RouterLink class="topbar-link" v-for="item in navStartItems" :to="{path: item}">
+          <RouterLink class="topbar-link" 
+          :class="{ 'topbar-link-active': activeItem === '/' + item, 'topbar-link-inactive': activeItem !== '/' + item }"
+          v-for="item in navStartItems" :to="{path: item}">
             {{ capitalize(item) }}
           </RouterLink>
 
         </div>
         <div class="topbar-end">
-          <RouterLink class="topbar-link" v-for="item in navEndItems" :to="{path: item}">
+          <RouterLink class="topbar-link" 
+          :class="{ 'topbar-link-active': activeItem === '/' + item, 'topbar-link-inactive': activeItem !== '/' + item }"
+          v-for="item in navEndItems" :to="{path: item}">
             {{ capitalize(item) }}
           </RouterLink>
         </div>
@@ -33,15 +47,13 @@ const capitalize = (str: String) => str.charAt(0).toUpperCase() + str.slice(1);
 
 
 <style scoped>
+
+  @import '../assets/base.css';
+
   .topbar-title {
     font-size: 1.5rem;
     font-weight: bold;
     margin: 0 2rem;
-    display: flex;
-    align-items: center;
-  }
-  .topbar-link {
-    padding: 0 1rem;
     display: flex;
     align-items: center;
   }
@@ -51,13 +63,31 @@ const capitalize = (str: String) => str.charAt(0).toUpperCase() + str.slice(1);
     align-items: stretch;
   }
 
-  /* On hover over a topbar-link, change color */
-  .topbar-link:hover {
+  
+  .topbar-link {
+    padding: 0 1rem;
+    display: flex;
+    align-items: center;
+  }
+
+  .topbar-link-active {
     background-color: #39487a;
     color: #e4e4e4;
+    transition: 0.25s;
+    transition-timing-function: ease-in-out;
+  }
+
+  .topbar-link-inactive {
+    transition: 0.25s;
+    transition-timing-function: ease-in-out;
+  }
+  
+  .topbar-link-inactive:hover {
+    background-color: #68749941;
     transition: 0.3s;
     transition-timing-function: ease-in-out;
   }
+
 
 
 
