d94504bf928c4ad829a77fccb5ff9fe41bdb69e1
keee99 <kohjj99@gmail.com>
2023-09-20 00:18:12 +0800

    Fix topbar styling for mobile view

--- src/components/Topbar.vue
+++ src/components/Topbar.vue
@@ -12,10 +12,10 @@ const props = defineProps({
 const navStartItems = ref(["about", "portfolio"]);
 const navEndItems = ref([]);
 const socials = ref([
-  {name: "github", link: "https://github.com/keee99"}, 
-  {name: "linkedin", link: "https://www.linkedin.com/in/jayjaykoh/"}, 
-  {name: "instagram", link: "https://www.instagram.com/jayjay_koh/"}, 
-  {name: "envelope", link: "mailto:kohjim99@gmail.com"},
+  { name: "github", link: "https://github.com/keee99" },
+  { name: "linkedin", link: "https://www.linkedin.com/in/jayjaykoh/" },
+  { name: "instagram", link: "https://www.instagram.com/jayjay_koh/" },
+  { name: "envelope", link: "mailto:kohjim99@gmail.com" },
 ]);
 const screenWidth = ref(window.innerWidth);
 const activeItem = toRef(props, "active");
@@ -31,235 +31,224 @@ window.addEventListener('resize', onResize);
 
 <template>
   <main>
-    <div class="app-topbar" v-if="screenWidth > screenThreshold ">
-        
-        <div class="topbar-start">
-          <a class="topbar-title neon-text-primary" href="#home">Jj</a>
-          
-          <a class="topbar-link" 
-            :class="{ 'topbar-link-active': activeItem === item, 'topbar-link-inactive': activeItem !== item }"
-            v-for="item in navStartItems"
-            :href="'#' + item">
-            <div class="topbar-text">
-              {{ capitalize(item) }}
-            </div>
-          </a>
+    <div class="app-topbar" v-if="screenWidth > screenThreshold">
 
-        </div>
-        <div class="topbar-end">
-          <a class="topbar-link" 
-            :class="{ 
-              'topbar-link-active': activeItem === item, 
-              'topbar-link-inactive': activeItem !== item 
-            }"
-            v-for="item in navEndItems" 
-            :href="'#' + item">
-            <div class="topbar-text">
-              {{ capitalize(item) }}
-            </div>
-          </a>
+      <div class="topbar-start">
+        <a class="topbar-title neon-text-primary" href="#home">Jj</a>
 
-          <a class="topbar-link" 
-            v-for="item in socials"
-            :href='item.link' target="_blank">
-            <i class="social-i pi" :class="'pi-' + item.name"></i>
-          </a>
-          
-          
-        </div>
-        
+        <a class="topbar-link"
+          :class="{ 'topbar-link-active': activeItem === item, 'topbar-link-inactive': activeItem !== item }"
+          v-for="item in navStartItems" :href="'#' + item">
+          <div class="topbar-text">
+            {{ capitalize(item) }}
+          </div>
+        </a>
+
+      </div>
+      <div class="topbar-end">
+        <a class="topbar-link" :class="{
+          'topbar-link-active': activeItem === item,
+          'topbar-link-inactive': activeItem !== item
+        }" v-for="item in navEndItems" :href="'#' + item">
+          <div class="topbar-text">
+            {{ capitalize(item) }}
+          </div>
+        </a>
+
+        <a class="topbar-link" v-for="item in socials" :href='item.link' target="_blank">
+          <i class="social-i pi" :class="'pi-' + item.name"></i>
+        </a>
+
+
+      </div>
+
     </div>
 
     <!-- Small screen size topbar -->
-    <div class="app-topbar" v-if="screenWidth <= 768 ">
-        
-        <div class="topbar-start">
-          <a class="topbar-title neon-text-primary" href="/#home">Jj</a>
-        </div>
-        <div class="topbar-end">
-          <Sidebar v-model:visible="sideBarVisible" position="full" id="app-sidebar" class="neon-border">
-            
-            <div class="sidebar-links">
-              <a class="topbar-link sidebar-col" 
-                :class="{ 'topbar-link-active': activeItem === item, 'topbar-link-inactive': activeItem !== item }"
-                v-for="item in navStartItems"
-                :href="'#' + item" 
-                @click="sideBarVisible=false">
-                <div class="topbar-text">
-                  {{ capitalize(item) }}
-                </div>
-              </a>
-            </div>
+    <div class="app-topbar" v-if="screenWidth <= 768">
 
-            <div class="sidebar-socials">
-              <a class="topbar-link sidebar-col" 
-                v-for="item in socials"
-                :href='item.link' target="_blank">
-                <i class="social-i pi" :class="'pi-' + item.name"></i>
-              </a>
-            </div>
-            
-          </Sidebar>
-          <i class="pi pi-bars topbar-hamburger neon-text-secondary" @click="sideBarVisible=true" />
-        </div>
-        
+      <div class="topbar-start">
+        <a class="topbar-title neon-text-primary" href="/#home">Jj</a>
+      </div>
+      <div class="topbar-end">
+        <Sidebar v-model:visible="sideBarVisible" position="full" id="app-sidebar" class="neon-border">
+
+          <div class="sidebar-links">
+            <a class="topbar-link sidebar-col"
+              :class="{ 'topbar-link-active': activeItem === item, 'topbar-link-inactive': activeItem !== item }"
+              v-for="item in navStartItems" :href="'#' + item" @click="sideBarVisible = false">
+              <div class="topbar-text">
+                {{ capitalize(item) }}
+              </div>
+            </a>
+          </div>
+
+          <div class="sidebar-socials">
+            <a class="topbar-link sidebar-col" v-for="item in socials" :href='item.link' target="_blank">
+              <i class="social-i pi" :class="'pi-' + item.name"></i>
+            </a>
+          </div>
+
+        </Sidebar>
+        <i class="pi pi-bars topbar-hamburger neon-text-secondary" @click="sideBarVisible = true" />
+      </div>
+
     </div>
-    
+
   </main>
 </template>
 
 
 <style scoped>
+@import '../assets/base.css';
 
-  @import '../assets/base.css';
+.topbar-title {
+  font-size: 1.5rem;
+  font-weight: bolder;
+  margin: 0 10% 0 5%;
+  display: flex;
+  align-items: center;
 
-  .topbar-title {
-    font-size: 1.5rem;
-    font-weight: bolder;
-    margin: 0 10% 0 5%;
-    display: flex;
-    align-items: center;
-
-    min-width: fit-content;
+  min-width: fit-content;
 
 
-    font-family: 'Beon', monospace;
+  font-family: 'Beon', monospace;
 }
 
 
-  .topbar-end, .topbar-start {
-    display: flex;
-    align-items: stretch;
-  }
+.topbar-end,
+.topbar-start {
+  display: flex;
+  align-items: stretch;
+}
 
-  
-  .topbar-link {
-    color: var(--color-text-header);
-    font-family: 'Beon', monospace;
-    padding: 0 1rem;
-    display: flex;
-    align-items: center;
-  }
 
-  .topbar-link-active {
+.topbar-link {
+  color: var(--color-text-header);
+  font-family: 'Beon', monospace;
+  padding: 0 1rem;
+  display: flex;
+  align-items: center;
+}
 
-    transition: 0.25s;
-    transition-timing-function: ease-in-out;
-  }
+.topbar-link-active {
 
-  .topbar-link-active div::after {
-    content: '';
-    position: absolute;
-    bottom: -2px;
-    left: 0;
-    width: 100%;
-    height: 2px;
-    background-color: var(--color-secondary-lighter);
-    box-shadow: 0 0 5px var(--color-secondary),
-                0 0 10px var(--color-secondary),
-                0 0 20px var(--color-secondary),
-                0 0 40px var(--color-secondary),
-                0 0 80px var(--color-secondary),
-                0 0 90px var(--color-secondary),
-                0 0 100px var(--color-secondary);
-    transition: 0.25s;
-    transition-timing-function: ease-in-out;
-  }
+  transition: 0.25s;
+  transition-timing-function: ease-in-out;
+}
 
-  .topbar-link-inactive {
-    transition: 0.15s;
-    transition-timing-function: ease-in-out;
-  }
+.topbar-link-active div::after {
+  content: '';
+  position: absolute;
+  bottom: -2px;
+  left: 0;
+  width: 100%;
+  height: 2px;
+  background-color: var(--color-secondary-lighter);
+  box-shadow: 0 0 5px var(--color-secondary),
+    0 0 10px var(--color-secondary),
+    0 0 20px var(--color-secondary),
+    0 0 40px var(--color-secondary),
+    0 0 80px var(--color-secondary),
+    0 0 90px var(--color-secondary),
+    0 0 100px var(--color-secondary);
+  transition: 0.25s;
+  transition-timing-function: ease-in-out;
+}
 
-  .social-i {
-    color: var(--color-secondary);
-    transition: 0.15s;
-    transition-timing-function: ease-in-out;
-  }
+.topbar-link-inactive {
+  transition: 0.15s;
+  transition-timing-function: ease-in-out;
+}
 
-  .topbar-link:hover .social-i {
-    color: var(--color-secondary-light);
-    transition: 0.15s;
-    transition-timing-function: ease-in-out;
-  }
+.social-i {
+  color: var(--color-secondary);
+  transition: 0.15s;
+  transition-timing-function: ease-in-out;
+}
 
-  /* .topbar-link:hover {
+.topbar-link:hover .social-i {
+  color: var(--color-secondary-light);
+  transition: 0.15s;
+  transition-timing-function: ease-in-out;
+}
+
+/* .topbar-link:hover {
     color: var(--color-text-focus);
     transition: 0.15s;
     transition-timing-function: ease-in-out;
   } */
 
-  .topbar-title:hover {
-    color: var(--color-lighter);
-    text-shadow: 0 0 5px var(--color-primary-lighter),
-                0 0 10px var(--color-primary-lighter),
-                0 0 20px var(--color-primary-light),
-                0 0 40px var(--color-primary-light),
-                0 0 80px var(--color-primary-light),
-                0 0 90px var(--color-primary-light),
-                0 0 100px var(--color-primary-light);
-    transition: 0.15s;
-    transition-timing-function: ease-in-out;
-  }
+.topbar-title:hover {
+  color: var(--color-lighter);
+  text-shadow: 0 0 5px var(--color-primary-lighter),
+    0 0 10px var(--color-primary-lighter),
+    0 0 20px var(--color-primary-light),
+    0 0 40px var(--color-primary-light),
+    0 0 80px var(--color-primary-light),
+    0 0 90px var(--color-primary-light),
+    0 0 100px var(--color-primary-light);
+  transition: 0.15s;
+  transition-timing-function: ease-in-out;
+}
 
-  .topbar-link-inactive:hover {
-    color: var(--color-text-dark);
-    transition: 0.15s;
-    transition-timing-function: ease-in-out;
-    background-color: var(--color-background-light);
-    box-shadow: 0 0 5px var(--color-secondary),
-                0 0 10px var(--color-secondary),
-                0 0 20px var(--color-secondary);
-  }
+.topbar-link-inactive:hover {
+  color: var(--color-text-dark);
+  transition: 0.15s;
+  transition-timing-function: ease-in-out;
+  background-color: var(--color-background-light);
+  box-shadow: 0 0 5px var(--color-secondary),
+    0 0 10px var(--color-secondary),
+    0 0 20px var(--color-secondary);
+}
 
-  .topbar-hamburger {
-    color: var(--color-secondary);
-    font-size: 1.5rem;
-    font-weight: bolder;
-    margin: 0 5% 0 0;
-    display: flex;
-    align-items: center;
-    cursor: pointer;
-  }
+.topbar-hamburger {
+  color: var(--color-secondary);
+  font-size: 1.5rem;
+  font-weight: bolder;
+  margin: 0 5% 0 0;
+  display: flex;
+  align-items: center;
+  cursor: pointer;
+}
 
-  .topbar-hamburger:hover {
-    color: white;
-    text-shadow: 0 0 5px var(--color-secondary-light),
-                0 0 10px var(--color-secondary-light),
-                0 0 20px var(--color-secondary-light);
-    transition-timing-function: ease-in-out;
-    transition: 0.15s;
-  }
+.topbar-hamburger:hover {
+  color: white;
+  text-shadow: 0 0 5px var(--color-secondary-light),
+    0 0 10px var(--color-secondary-light),
+    0 0 20px var(--color-secondary-light);
+  transition-timing-function: ease-in-out;
+  transition: 0.15s;
+}
 
-  .topbar-link.sidebar-col {
-    font-size: 1.5rem;
-    width: 100%;
-    padding-bottom: 1rem;
-    display: flex;
-    justify-content: center;
+.topbar-link.sidebar-col {
+  font-size: 1.2rem;
+  width: 100%;
+  padding: 0.5rem;
+  display: flex;
+  justify-content: center;
+}
 
-  }
+.sidebar-links+.sidebar-socials {
+  border-top: 1px solid var(--color-text-dark);
+  padding-top: 1rem;
+}
 
-  .sidebar-links + .sidebar-socials {
-    border-top: 1px solid var(--color-text-dark);
-    padding-top: 5rem;
-  }
-  .sidebar-socials {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    flex-wrap: wrap;
-  }
+.sidebar-socials {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-wrap: wrap;
+}
 
-  .sidebar-socials .topbar-link.sidebar-col {
-    font-size: 3rem;
-    height: 4.5rem;
+.sidebar-socials .topbar-link.sidebar-col {
+  font-size: 3rem;
+  height: 3rem;
 
-  }
-  .sidebar-socials .topbar-link.sidebar-col i {
-    font-size: 1.5rem;
+}
 
-  }
+.sidebar-socials .topbar-link.sidebar-col i {
+  font-size: 1.2rem;
 
-
+}
 </style>
